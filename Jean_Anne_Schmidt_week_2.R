4b2b65f669e82e74f97935620e44c13647ee076c
jeananneschmidt <44173139+jeananneschmidt@users.noreply.github.com>
2018-11-04 14:35:15 -0500

    Update Jean_Anne_Schmidt_week_2.R

--- Jean_Anne_Schmidt_week_2.R
+++ Jean_Anne_Schmidt_week_2.R
@@ -9,30 +9,40 @@ ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy))
 (filter(mpg, cyl == 8))
 (filter(diamonds, carat > 3))
-install.packages(c("nycflights13", "gapminder", "Lahman"))
+#install.packages(c("nycflights13", "gapminder", "Lahman"))
+library(nycflights13)
+library(dplyr)
+
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy))
 nycflights13::flights
 (top <- head(nycflights13::flights))
 arrival.delay <- filter(nycflights13::flights, arr_delay >= 120)
-Houston <- filter(nycflights13::flights, dest=="IAH" | dest=="HOU")
-select.carrier <- filter(nycflights13::flights, carrier=="UA" | carrier=="AA" | carrier=="DL")
-summer.depart <- filter(nycflights13::flights, month==7 | month==8 | month==9)
-arrlate <- filter(nycflights13::flights, arr_delay > 120 & dep_delay <= 0)
-deplate <- filter(nycflights13::flights, dep_delay >= 60 & arr_delay <=30)
-flight.time <- filter(nycflights13::flights, dep_time >= 0 & dep_time <= 600)
-testvar.between <- filter(nycflights13::flights, between(dep_time, 0, 600))
-filter(df, is.na(dep_time))
-#How many flights have a missing departure time
-#why is NA ^ 0 not missing
+arrival.delay <- filter(flights, arr_delay >= 120)
+Houston <- filter(flights, dest=="IAH" | dest=="HOU")
+select.carrier <- filter(flights, carrier=="UA" | carrier=="AA" | carrier=="DL")
+summer.depart <- filter(flights, month==7 | month==8 | month==9)
+arrlate <- filter(flights, arr_delay > 120 & dep_delay <= 0)
+deplate <- filter(flights, dep_delay >= 60 & arr_delay < 30)
+flight.time <- filter(flights, dep_time >= 0 & dep_time <= 600)
+testvar.between <- filter(flights, between(dep_time, 0, 600))
+filter(flights, is.na(dep_time))
+
+
 #How could you use arrange() to sort all missing values to the start? (Hint: use is.na()).
-arrange(nycflights13::flights, desc(dep_delay))
-arrange(nycflights13::flights, dep_time)       
-arrange(nycflights13::flights, desc(dep_time))
-arrange(nycflights13::flights, air_time)
-arrange(nycflights13::flights, desc(air_time))
+arrange(flights, desc(is.na(dep_delay)))
+arrange(flights, desc((dep_delay)))
+arrange(flights, dep_time)       
+arrange(flights, desc(dep_time))
+arrange(flights, air_time)
+longest <- arrange(flights, desc(distance))
+shortest <- arrange(flights, distance)
+
 select(nycflights13::flights, year, year)
+# it only uses the variable once
+
 #one_of matches variable names in a character vector
-select(nycflights13::flights, one_of(c("carrier", "month")))
-select(nycflights13::flights, contains("TIME"))
-#^returns all variables that contaim "time"
+select(flights, one_of(c("carrier", "month")))
+select(flights, contains("TIME"))
+select(flights, contains("TIME", ignore.case = FALSE))
+#^returns all variables that contain "time" You can change the default to ignore case
